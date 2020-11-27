3b9482d7ce0e3111b45e7fa10a020e0f7acf2b0d
Sofron80 <Sofron80@users.noreply.github.com>
2020-11-27 08:46:04 +0600

    realise resize tables

---
+++ src/components/table/table.resize.js
@@ -0,0 +1,45 @@
+import { $ } from '@core/dom'
+
+export function resizeHandler(e, $root) {
+  const $resizer = $(e.target)
+  const $parent = $resizer.closest('[data-type="resizeable"]')
+  const coords = $parent.getCoords()
+  const cells = $root.findAll(`[data-col="${$parent.data.col}"]`)
+  const type = $resizer.data.resize
+  let value
+
+  const cssProp = type === 'col' ? 'bottom' : 'right'
+
+  $resizer.css({
+    opacity: 1,
+    [cssProp]: '-5000px',
+  })
+
+  document.onmousemove = e => {
+    if (type === 'col') {
+      const delta = e.pageX - coords.right
+      value = coords.width + delta
+      $resizer.css({ right: -delta + 'px' })
+    } else {
+      const delta = e.pageY - coords.bottom
+      value = coords.height + delta
+      $resizer.css({ bottom: -delta + 'px' })
+    }
+  }
+
+  document.onmouseup = e => {
+    document.onmousemove = null
+    document.onmouseup = null
+    if (type === 'col') {
+      $parent.css({ width: value + 'px' })
+      cells.forEach(el => $(el).css({ width: value + 'px' }))
+    } else {
+      $parent.css({ height: value + 'px' })
+    }
+    $resizer.css({
+      opacity: null,
+      bottom: 0,
+      right: 0,
+    })
+  }
+}
--- src/core/dom.js
+++ src/core/dom.js
@@ -31,6 +31,27 @@ class Dom {
     }
     return this
   }
+  closest(selector) {
+    return $(this.$el.closest(selector))
+  }
+  get data() {
+    return this.$el.dataset
+  }
+
+  getCoords() {
+    return this.$el.getBoundingClientRect()
+  }
+  findAll(selector) {
+    return this.$el.querySelectorAll(selector)
+  }
+
+  css(styles = {}) {
+    console.log(styles)
+    Object.keys(styles).forEach(key => {
+      this.$el.style[key] = styles[key]
+    })
+    return this
+  }
 }
 
 export function $(selector) {
